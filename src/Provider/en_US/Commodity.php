8799633ed78038cfdaf44e44c41e8f15b82b9c81
Ryan Leach <leachryan@hotmail.com>
2019-05-01 22:00:33 -0500

    Update Commodity.php

--- src/Provider/en_US/Commodity.php
+++ src/Provider/en_US/Commodity.php
@@ -5,7 +5,30 @@ namespace FakerCommodities\Provider\en_US;
 class Commodity extends \Faker\Provider\Base
 {
     protected static $commodities = [
-        'Corn', 'Soybeans', 'Sorghum', 'Durum Wheat', 'Hard Red Winter Wheat', 'Spring Wheat', 'Soft Red Winter Wheat', 'White Wheat', 'Barley', 'Millet', 'Oats',
+        'Corn', 
+        'Mini-Corn',
+        'Soybeans', 
+        'Soybean Oil',
+        'Mini Soybeans',
+        'Sorghum', 
+        'Durum Wheat', 
+        'Hard Red Winter Wheat', 
+        'Spring Wheat', 
+        'Soft Red Winter Wheat', 
+        'White Wheat', 
+        'Barley', 
+        'Millet', 
+        'Oats',
+        'Rough Rice',
+        'Canola',
+        'Cotton',
+        'Palm Oil',
+        'Cocoa',
+        'Coffee',
+        'Sugar',
+        'Lumbar',
+        'Rubber',
+        'Wool'
     ];
 
     /**
